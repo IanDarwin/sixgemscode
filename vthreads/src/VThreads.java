d7277dbe7ada4933bf57e571808d7ae141365aa2
Ian Darwin <ian@darwinsys.com>
2025-01-20 18:47:19 -0500

    Inline bits, better messaging, comments

--- vthreads/src/VThreads.java
+++ vthreads/src/VThreads.java
@@ -3,8 +3,12 @@ import java.nio.file.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/// Benchmark platform threads vs virtual threads
+/// Never trust someone else's benchmarks - profile
+/// your own application instead!
 public class VThreads {
-    final BufferedWriter out = Files.newBufferedWriter(Path.of("/tmp/id"));
+    final BufferedWriter out =
+            Files.newBufferedWriter(Path.of("/tmp/id"));
     Runnable ioBoundRunnable = () -> {
         try {
             out.write("A");
@@ -12,9 +16,9 @@ public class VThreads {
             throw new RuntimeException(e);
         }
     };
-    int number = 0;
+    public static int number = 0;
     Runnable cpuBoundRunnable = () -> {
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 100000; i++) {
             number++;
         }
     };
@@ -24,20 +28,17 @@ public class VThreads {
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome to Virtual Threads!");
         VThreads demo = new VThreads();
-        ExecutorService oldPool, newPool;
-        oldPool = Executors.newCachedThreadPool();
+        ExecutorService oldPool = Executors.newCachedThreadPool();
         demo.takeTime("Traditional Threads, IO busy", oldPool, demo.ioBoundRunnable);
 
-        newPool = Executors.newVirtualThreadPerTaskExecutor();
+        ExecutorService newPool = Executors.newVirtualThreadPerTaskExecutor();
         demo.takeTime("Virtual Threads, IO busy", newPool, demo.ioBoundRunnable);
 
-        oldPool = Executors.newCachedThreadPool();
-        demo.takeTime("Traditional Threads, CPU bound", oldPool, demo.cpuBoundRunnable);
+        demo.takeTime("Traditional Threads, CPU bound", Executors.newCachedThreadPool(), demo.cpuBoundRunnable);
 
-        newPool = Executors.newVirtualThreadPerTaskExecutor();
-        demo.takeTime("Virtual Threads, CPU bound", newPool, demo.cpuBoundRunnable);
+        demo.takeTime("Virtual Threads, CPU bound", Executors.newVirtualThreadPerTaskExecutor(), demo.cpuBoundRunnable);
     }
 
     public void takeTime(String descr, ExecutorService pool, Runnable r) throws IOException {
